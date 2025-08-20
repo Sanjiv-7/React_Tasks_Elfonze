5e5daf0298669e9473ca2eba2c5f4a1b7313237c
Vinodkumar Sanjiv <vinodkumar.s@elfonze.com>
2025-08-20 15:01:17 +0530

    Added 4 react Tasks from Day 1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Message from './Task1_1'
+import Counter from './Task1_2';
+import LoginStatus from './Task1_3';
+import FruitList from './Task1_4';
+
+
+function App(){
+
+  return (<div><Message /><Counter /><LoginStatus /><FruitList /></div>)
+  
+}
+
+
+
+export default App;
